365d42fe114add3c8f0aab3d8d82edf8438a3114
lilyen06 <74439321+lilyen06@users.noreply.github.com>
2023-05-02 21:05:08 -0400

    Initial GOL program

---
+++ main.js
@@ -0,0 +1,39 @@
+"use strict;" 
+
+var conway;//must be declared in the global scope to be accessed via HTML onclick attibute
+
+var can = document.querySelector("canvas"); //reference to our canvas
+
+var mousedown = false;
+
+//main function is called on windowload event
+window.addEventListener("load", function(event) {
+
+	//instantiate game (columns, rows, cellsize, toroidal overlap)
+	conway = new Game(50, 40, 7, false);
+	conway.initialize();
+
+});
+
+can.addEventListener("mousedown", function(event){
+
+	updateCanvasCell(can, event, 1, conway.grid.columns, conway.grid.rows);
+	mousedown = true;
+
+});
+
+can.addEventListener("mousemove", function(event){
+
+	if(mousedown)updateCanvasCell(can, event, 1, conway.grid.columns, conway.grid.rows);
+	
+
+});
+
+can.addEventListener("mouseup", function(event){
+
+	mousedown = false;
+	lastCellX = null;
+	lastCellY = null;
+
+});
+
