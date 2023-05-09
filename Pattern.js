bd84cee73052ab80196da0c1818257d9085633c2
lilyen06 <74439321+lilyen06@users.noreply.github.com>
2023-05-09 13:19:11 -0400

    fix doubling up on pattern

--- Pattern.js
+++ Pattern.js
@@ -10,21 +10,18 @@ class Pattern{
     }
 
     randomize(){
-        let xlast = null;
-        let ylast = null;
         for (let i=0;i<this.numAlive;i++){
             let xrand = Math.floor(Math.random()*this.cols);
             let yrand = Math.floor(Math.random()*this.rows);
-            while(xrand==xlast && yrand==ylast){
-                xrand = Math.floor(Math.random()*this.cols);
-                yrand = Math.floor(Math.random()*this.rows);
-                console.log("repeat");
-            }
             let y = yrand+Math.floor(this.game.rows/2-this.rows/2)
             let x = xrand+Math.floor(this.game.columns/2-this.cols/2)
+            while(this.grid.isOn(y,x)){
+                xrand = Math.floor(Math.random()*this.cols);
+                yrand = Math.floor(Math.random()*this.rows);
+                y = yrand+Math.floor(this.game.rows/2-this.rows/2)
+                x = xrand+Math.floor(this.game.columns/2-this.cols/2)
+            }
             this.grid.turnOn(y,x);
-            xlast = xrand;
-            ylast = yrand;
         }
     }
 }
